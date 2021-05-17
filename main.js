61eab20370055a506896831dd22b47f8630f4243
Nagy Noel <nagynooel@gmail.com>
2021-05-17 15:53:16 +0200

    Added content, styling and basic javascript to products.html

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ function showSuccess(){
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+function toggleAllBox(source) {
+    checkboxes = document.getElementsByClassName('products-checkbox');
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+        checkboxes[i].checked = source.checked;
+    }
+}
+
+function setCheckAll() {
+    document.querySelector('input.products-checkbox-all').checked =
+    document.querySelectorAll('.products-checkbox').length ==
+    document.querySelectorAll('.products-checkbox:checked').length;
+}
